246694bb0cc53f7951e6e7d37ffc26c978b7b953
Napathsara Pinthong <napathsara.pin@gmail.com>
2024-10-30 22:29:10 +0700

    feat: set up firebase

---
+++ FirebaseConfig.tsx
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { API_KEY,AUTH_DOMAIN, DB_URL, STORAGE_BUCKET } from "@env";
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DB_URL,
+  projectId: "bookstoremobileapp",
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: "559210747126",
+  appId: "1:559210747126:web:49adc58181e13aba8848d7"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
---
+++ expo-env.d.ts
@@ -0,0 +1,7 @@
+declare module '@env' {
+    export const API_KEY: string;
+    export const AUTH_DOMAIN: string;
+    export const DB_URL: string;
+    export const STORAGE_BUCKET: string;
+  }
+  
